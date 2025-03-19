8aad199ff74fd2fc8bae459d93b1f2851bc87779
whim <1492490173@qq.com>
2025-03-19 15:18:58 +0800

    统一ems

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,22 +11,17 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
-
-module.exports = {
-  devServer: {
-    proxy: {
+  server: { // 替换 devServer 为 server
+    proxy: { // 直接在 server 下配置 proxy
       '/api': {
         target: 'http://192.168.1.208:9006', // 后端接口地址
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  }
-}
+        changeOrigin: true, // 允许跨域
+        ws: true, // 支持 WebSocket
+        rewrite: (path) => path.replace(/^\/api/, ''), // 重写路径，替换 /api 为 ''
+      },
+    },
+  },
+});
